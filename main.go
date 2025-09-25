7353815dea3fd58168e0cf50b85422226a4ae503
Fernandes Samuel <glazvonkrovi@gmail.com>
2025-09-25 17:04:42 +0200

    format code

--- main.go
+++ main.go
@@ -6,23 +6,20 @@ import (
 )
 
 func main() {
-	// Check if we have the right number of arguments
+
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: gorr release <patch|minor|major>")
 		fmt.Println("Example: gorr release patch")
 		os.Exit(1)
 	}
 
-	// Check if the first argument is "release"
 	if os.Args[1] != "release" {
 		fmt.Printf("Error: Unknown command '%s'. Use 'gorr release <patch|minor|major>'\n", os.Args[1])
 		os.Exit(1)
 	}
 
-	// Get the release type from the second argument
 	releaseType := os.Args[2]
 
-	// Validate the release type
 	validTypes := map[string]bool{
 		"patch": true,
 		"minor": true,
@@ -34,6 +31,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Display the chosen option
 	fmt.Printf("Release type chosen: %s\n", releaseType)
 }
