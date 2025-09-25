a30c36e8ad1c62e3a3c8334b72f396076522c02d
Fernandes Samuel <glazvonkrovi@gmail.com>
2025-09-25 17:04:25 +0200

    register release command

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	// Define a command-line flag
-	dice := flag.String("d", "d6", "Specify the type of dice to roll. Format is DX where X is an integer. Default is D6.")
-	flag.Parse()
-	fmt.Printf("You rolled a %s\n", *dice)
+	// Check if we have the right number of arguments
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: gorr release <patch|minor|major>")
+		fmt.Println("Example: gorr release patch")
+		os.Exit(1)
+	}
+
+	// Check if the first argument is "release"
+	if os.Args[1] != "release" {
+		fmt.Printf("Error: Unknown command '%s'. Use 'gorr release <patch|minor|major>'\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	// Get the release type from the second argument
+	releaseType := os.Args[2]
+
+	// Validate the release type
+	validTypes := map[string]bool{
+		"patch": true,
+		"minor": true,
+		"major": true,
+	}
+
+	if !validTypes[releaseType] {
+		fmt.Printf("Error: Invalid release type '%s'. Must be one of: patch, minor, major\n", releaseType)
+		os.Exit(1)
+	}
+
+	// Display the chosen option
+	fmt.Printf("Release type chosen: %s\n", releaseType)
 }
